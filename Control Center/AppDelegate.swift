7206159dd89a9e64834a1e62d5d3e4f8ca1fd3dc
Minh-Ton <ford.tonthat@gmail.com>
2020-07-08 21:54:10 +0700

    Initial Commit

---
+++ Control Center/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Control Center
+//
+//  Created by Ford on 7/8/20.
+//  Copyright © 2020 fordApps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
